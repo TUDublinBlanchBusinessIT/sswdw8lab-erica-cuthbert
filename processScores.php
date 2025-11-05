8aaa1bc659a36a8f1ea0d3614d4e576cf18545f2
B00162515 Erica Cuthbert <B00162515@mytudublin.ie>
2025-11-05 11:10:45 +0000

    Part 3 fixed

--- processScores.php
+++ processScores.php
@@ -6,7 +6,7 @@ $myTeam->finalScore($_GET[‘home1’],$_GET[‘away1’]);
 $myTeam->finalScore($_GET[‘home2’],$_GET[‘away2’]);
 $myTeam->finalScore($_GET[‘home3’],$_GET[‘away3’]);
 
-echo “The teams goal average is” . $myTeam->getGoalAverage()
+echo "The teams goal average is" . $myTeam->getGoalAverage();
 
 
 ?>
